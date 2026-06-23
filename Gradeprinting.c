8182bfaabe6c2b68e7aaa30f7e564233e8bba82f
sagardalvi18 <sagardalvi2122@gmail.com>
2026-06-23 19:59:54 +0530

    Add files via upload

---
+++ Gradeprinting.c
@@ -0,0 +1,38 @@
+// Experiment No 4
+// Name-Sagar Ravindra Dalvi
+//PRN- 2503033111372L007
+#include <stdio.h>
+
+int main() {
+    char grade;
+
+    printf("Enter Grade (A/B/C/D/F): ");
+    scanf(" %c", &grade);
+
+    switch(grade) {
+        case 'A':
+            printf("Excellent");
+            break;
+
+        case 'B':
+            printf("Very Good");
+            break;
+
+        case 'C':
+            printf("Good");
+            break;
+
+        case 'D':
+            printf("Pass");
+            break;
+
+        case 'F':
+            printf("Fail");
+            break;
+
+        default:
+            printf("Invalid Grade");
+    }
+
+    return 0;
+}
